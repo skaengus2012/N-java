de23edbb886cdea99cc25b884943f12c9e8db7fd
Doohyun Nam <skaengus2012@naver.com>
2017-03-21 03:40:08 +0900

    #1 java7 define.

--- N-java/src/main/java/Njava/util/ads.java
+++
@@ -1,7 +0,0 @@
-package Njava.util;
-
-/**
- * Created by Doohyun on 2017. 3. 20..
- */
-public class ads {
-}
--- N-java/src/test/java/runTest/Test.java
+++ N-java/src/test/java/runTest/Test.java
@@ -19,6 +19,5 @@ public class Test {
                 System.out.println(integer);
             }
         });
-
     }
 }
