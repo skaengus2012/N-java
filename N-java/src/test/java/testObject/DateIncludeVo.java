e8d9451b23109f3a40e1e1c7f958c67c7890927e
Doohyun Nam <skaengus2012@naver.com>
2017-07-26 17:35:10 +0900

    #1 StringUtil::ToJson 추가.

---
+++ N-java/src/test/java/testObject/DateIncludeVo.java
@@ -0,0 +1,20 @@
+package testObject;
+
+import java.util.Date;
+
+/**
+ * Date 를 포함하는 VO 정의.
+ *
+ * Created by Doohyun on 2017. 7. 26..
+ */
+public class DateIncludeVo {
+    private Date date;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
